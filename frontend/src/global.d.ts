adc0b21a8c2de85109509433ff9edbf7d9d709a3
vnimrod <nimrod.weins@gmail.com>
2025-08-27 00:23:35 +0300

    add main page nav style, page clipping background and img

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -4,3 +4,23 @@ declare module '*.module.scss' {
 }
 
 declare module '*.scss';
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
